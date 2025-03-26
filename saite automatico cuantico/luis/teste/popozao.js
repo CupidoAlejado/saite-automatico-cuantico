0c045c4bf312953c3a991b745ba5e2a8fd570bd7
Luis Fernano <149402596+CupidoAlejado@users.noreply.github.com>
2025-03-26 12:47:52 -0300

    Add files via upload

---
+++ saite automatico cuantico/luis/teste/popozao.js
@@ -0,0 +1,12 @@
+function entrar(x) {
+    //document.getElementById(x).style.display = 'block'
+
+    $('.conteudo').load('./docs/doc'+x+'.html');
+    
+    //     console.log(url);
+    //     $('.1').attr('src', url);
+}
+
+function alertar() {
+    alert('deu certouuu')
+}
---
+++ saite automatico cuantico/site automatico robotizado/main.py
@@ -0,0 +1,8 @@
+import time
+import pyautogui as pg
+import keyboard
+from pyscript import document
+
+# def matarsite(event):
+print('la ele 1000x')
+pg.hotkey("ctrlleft","w")
